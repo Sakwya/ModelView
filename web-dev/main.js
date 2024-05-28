0af2bf88bc968cdc4eac3c756f6d424bd5fcacde
Skuyazlu <celiasash@foxmail.com>
2024-05-28 11:34:45 +0800

    1

--- web-dev/main.js
+++ web-dev/main.js
@@ -1,4 +1,25 @@
-function main() {
+const light_setup = () => {
+	const light_array = []
+	let ambientLight = new THREE.AmbientLight(0x404040, 2); // 白色环境光
+	let directionalLight0 = new THREE.DirectionalLight(0xffffff, 1);
+	directionalLight0.position.set(1, 1, 1).normalize();
+	let directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+	directionalLight1.position.set(-1, 1, 1).normalize();
+	let directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+	directionalLight2.position.set(1, 1, -1).normalize();
+	let directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
+	directionalLight3.position.set(-1, 1, -1).normalize();
+	const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+	light_array.push(ambientLight)
+	light_array.push(directionalLight0)
+	light_array.push(directionalLight1)
+	light_array.push(directionalLight2)
+	light_array.push(directionalLight3)
+	light_array.push(hemisphereLight)
+	return light_array
+}
+
+const main = () => {
 	let scene = new THREE.Scene();
 	let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -6,16 +27,62 @@ function main() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	// 添加一个简单的立方体
-	let geometry = new THREE.BoxGeometry();
-	let material = new THREE.MeshBasicMaterial({
-		color: 0x00ff00
-	});
-	let cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
+	let currentModel = new THREE.Object3D();
+	let controls = new THREE.OrbitControls(camera, renderer.domElement);
+	scene.background = new THREE.Color(0x404040); // 灰色背景
 
-	camera.position.z = 5;
+	const light_array = light_setup()
+	for (const light of light_array) {
+		scene.add(light)
+	}
+	const light_intensity = (intensity) => {
+		for (const light of light_array) {
+			light.intensity = intensity
+		}
+	}
 
+	function calculateScaleToFit(model) {
+		const box = new THREE.Box3().setFromObject(model);
+		const size = new THREE.Vector3();
+		let center = new THREE.Vector3();
+		box.getSize(size);
+		const maxDimension = Math.max(size.x, size.y, size.z);
+		center.x = size.x / 2;
+		center.y = size.y / 2;
+		center.z = size.z / 2;
+		window.x = size.x/maxDimension
+		window.y = size.y/maxDimension
+		window.z = size.z/maxDimension
+		window.model = model
+		// model.position.copy(center);
+		const scale = 1 / maxDimension;
+		model.scale.set(scale, scale, scale);
+		const group = new THREE.Group();
+		group.add(model);
+		// group.position.copy(center);
+		// console.log(group)
+		return group
+	}
+
+	// 加载模型的函数
+	const loadModel = async function(modelUrl) {
+		return new Promise((resolve, reject) => {
+
+			if (currentModel) {
+				scene.remove(currentModel);
+			}
+
+			let loader = new THREE.GLTFLoader();
+			loader.load(modelUrl, function(gltf) {
+				temp = calculateScaleToFit(gltf.scene)
+				// console.log(temp)
+				currentModel = temp
+				scene.add(currentModel);
+			});
+			resolve()
+		})
+	}
+	camera.position.z = 2;
 	var animationRunning = true
 	var rotation = {
 		'x': 0,
@@ -27,10 +94,12 @@ function main() {
 			return
 		}
 		requestAnimationFrame(animate);
-		cube.rotation.x += 0.1 * (rotation.x - cube.rotation.x);
-		cube.rotation.y += 0.1 * (rotation.y - cube.rotation.y);
-		cube.rotation.z += 0.1 * (rotation.z - cube.rotation.z);
-
+		// currentModel.rotation.x += 0.1 * (rotation.x - currentModel.rotation.x);
+		// currentModel.rotation.y += 0.1 * (rotation.y - currentModel.rotation.y);
+		// currentModel.rotation.z += 0.1 * (rotation.z - currentModel.rotation.z);
+		currentModel.rotation.x +=0.025
+		currentModel.rotation.y +=0.05
+		currentModel.rotation.z +=0.1
 		renderer.render(scene, camera);
 	};
 	const stopRender = function() {
@@ -42,19 +111,29 @@ function main() {
 			return false
 		}
 		animationRunning = true
+		controls.update();
+
 		requestAnimationFrame(animate)
 
 	}
 	const setRotation = function(x, y, z) {
+		rotation.x += (x - rotation.x) % (2 * Math.PI)
+		rotation.y += (y - rotation.y) % (2 * Math.PI)
+		rotation.z += (z - rotation.z) % (2 * Math.PI)
+	}
+	const setRawRotation = function(x, y, z) {
 		rotation.x = x
 		rotation.y = y
 		rotation.z = z
 	}
-	animate();
+	loadModel("./assets/blue_archivekasumizawa_miyu/scene.gltf").then(animate())
+	// loadModel("./assets/city.glb").then(animate())
 	return {
 		startRender,
 		stopRender,
 		setRotation,
+		setRawRotation,
+		light_intensity
 	}
 }
 const render = main()
